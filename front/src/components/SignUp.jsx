64685a89ec72d2bc700ebc7fb364a6b30a7b00af
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-11 10:57:35 +0200

    Add form inputs and handle

--- front/src/components/SignUp.jsx
+++ front/src/components/SignUp.jsx
@@ -5,27 +5,52 @@ export default class SignUp extends Component {
     super(props);
     this.state = {
       email: "",
+      password: "",
+      verifyPassword: "",
+      name: "",
+      lastname: "",
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.createInput = this.createInput.bind(this);
   }
 
   handleChange = (e) => {
     this.setState({
-      email: e.target.value,
+      [e.target.name]: e.target.value,
     });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  createInput = (name, type, placeholder = "") => {
+    return (
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={this.state[name]}
+        onChange={this.handleChange}
+      />
+    );
   };
 
   render() {
     return (
       <>
-        <h1>{this.state.email}</h1>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={this.state.email}
-          onChange={this.handleChange}
-        />
+        <h1>{JSON.stringify(this.state)}</h1>
+        <form action="" onSubmit={this.handleSubmit}>
+          {this.createInput("email", "email", "Email")}
+          {this.createInput("password", "password", "Password")}
+          {this.createInput("verifyPassword", "password", "Verify Password")}
+          {this.createInput("name", "text", "Name")}
+          {this.createInput("lastname", "text", "Lastname")}
+          <button type="submit">Sign Up</button>
+        </form>
       </>
     );
   }
