ced0b2c9b8a496ee2e13bbd692b4f0ec0c238914
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-12 15:04:02 +0200

    Add error handling for signup route

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-  res.send("I am in POST signup");
+  const error = null;
+  if (error) {
+    res.status(400).json({ flash: error.message });
+  }
+  res.status(200).json({ flash: "User has been signed up !" });
 });
 
 module.exports = router;
